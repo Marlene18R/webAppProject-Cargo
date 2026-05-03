2028c7ea817948609d1d2cbab9fdb1e071803888
Marlene_Ramos <a01646342@tec.mx>
2026-05-03 12:47:29 -0600

    Prettier fixed

--- backend-mep/server.js
+++ backend-mep/server.js
@@ -47,9 +47,9 @@ app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("JWT_SECRET:", process.env.JWT_SECRET);
-console.log("JWT_EXPIRES_IN:", process.env.JWT_EXPIRES_IN);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('JWT_SECRET:', process.env.JWT_SECRET);
+console.log('JWT_EXPIRES_IN:', process.env.JWT_EXPIRES_IN);
